d8dc78b535d0a9bc8f11d205c7997267c943a416
Destingem <80842481+Destingem@users.noreply.github.com>
2022-04-26 11:42:10 +0200

    dv aaded

--- Components/DefaultStyles.js
+++ Components/DefaultStyles.js
@@ -5,18 +5,27 @@ import Head from "next/head";
 import RegisterInfo from "./RegisterInfo/RegisterInfo";
 import { useDispatch, useSelector } from "react-redux";
 import { useMetaMask } from "metamask-react";
-import styles from "./DefaultStyles.module.css"
+import styles from "./DefaultStyles.module.css";
 export default function DefaultStyles(props) {
   try {
-    var modals = useSelector(state => {return state.modals})
-    var state = useSelector(state => state)
+    var modals = useSelector((state) => {
+      return state.modals;
+    });
+    var state = useSelector((state) => state);
   } catch (error) {
     console.log(error);
   }
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@700&family=Roboto:wght@300&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       {props.children}
-    
     </>
   );
 }
